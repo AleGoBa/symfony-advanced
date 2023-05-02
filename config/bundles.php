36eee5d9df7e73fbfe6d8705335ccf728990e907
AleGoBa <davidalejandrogonzalezbarba@gmail.com>
2023-05-02 16:55:05 -0600

    feat: :sparkles: Easy admin bundle added

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
